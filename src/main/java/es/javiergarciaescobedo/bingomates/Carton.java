c3e8c5da26449f88ea3bab0099c21794890d6763
Javier García Escobedo <javiergarbedo@gmail.com>
2021-02-12 10:18:41 +0100

    Día 2 - Método mostrar cartón y rellenar con aleatorios

--- src/main/java/es/javiergarciaescobedo/bingomates/Carton.java
+++ src/main/java/es/javiergarciaescobedo/bingomates/Carton.java
@@ -4,11 +4,34 @@ import java.util.Random;
 
 public class Carton {
     
+    int[][] nums;
+            
     public void generar() {
-        int[][] num = new int[9][3]
+        nums = new int[9][3];
+        
+        for(int y=0; y<3; y++) {
+            for(int x=0; x<9; x++) {
+                nums[x][y] = getNumAleatorio(1, 90);
+            }
+        }
+        
+        this.mostrarPorConsola();
+    }
+    
+    public void mostrarPorConsola() {
+        for(int y=0; y<3; y++) {
+            for(int x=0; x<9; x++) {
+                //System.out.println("x=" + x + " y=" + y);
+                System.out.print(nums[x][y] + " ");
+            }
+            System.out.println();
+        }        
+    }
+    
+    public int getNumAleatorio(int min, int max) {
         Random random = new Random();
-        int num = random.nextInt(90)+1;
-        System.out.println(num);
+        int num = random.nextInt(max-min+1) + min;
+        return num;
     }
     
 }
