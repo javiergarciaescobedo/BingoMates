49fbc93c75b5279fe377e94952f5328ebd91d741
Javier García Escobedo <javiergarbedo@gmail.com>
2021-03-09 09:33:36 +0100

    Panel superior con generador de bolas

--- src/main/java/es/javiergarciaescobedo/bingomates/App.java
+++ src/main/java/es/javiergarciaescobedo/bingomates/App.java
@@ -1,8 +1,14 @@
 package es.javiergarciaescobedo.bingomates;
 
+import java.util.Random;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,9 +28,26 @@ public class App extends Application {
         
         CartonView cartonView = new CartonView(carton);
         paneRoot.setCenter(cartonView);
-        
+
+        // Contenedor parte superior
+        HBox hboxTop = new HBox();
+        hboxTop.setAlignment(Pos.CENTER);
+        hboxTop.setSpacing(10);
+        hboxTop.setStyle("-fx-border-color: green");
+        paneRoot.setTop(hboxTop);        
+        // Texto
+        Label labelMsgSorteo = new Label("Número: ");
+        hboxTop.getChildren().add(labelMsgSorteo);
+        // Bola con número
         Bola bola = new Bola();
-        paneRoot.setTop(bola);
+        hboxTop.getChildren().add(bola);
+        // Botón para generar número
+        Button buttonNumero = new Button("Extraer número");
+        Random random = new Random();
+        buttonNumero.setOnAction((ActionEvent e) -> {
+            bola.setNumero(random.nextInt(90)+1);            
+        });
+        hboxTop.getChildren().add(buttonNumero);
     }
     
     public void mostrarNum(int num) {
--- src/main/java/es/javiergarciaescobedo/bingomates/Bola.java
+++ src/main/java/es/javiergarciaescobedo/bingomates/Bola.java
@@ -1,21 +1,27 @@
 package es.javiergarciaescobedo.bingomates;
 
-import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class Bola extends Group {
+public class Bola extends StackPane {
     
+    Label labelBola;
+            
     public Bola() {
         Circle circleBola = new Circle();
         circleBola.setRadius(20);
         circleBola.setFill(Color.LIGHTGRAY);
         this.getChildren().add(circleBola);
         
-        Label labelBola = new Label();
-        labelBola.setText("23");
+        labelBola = new Label();
+        labelBola.setStyle("-fx-border-color: red");
         this.getChildren().add(labelBola);
+    }
+    
+    public void setNumero(int num) {
+        labelBola.setText(String.valueOf(num));
     }
     
     
